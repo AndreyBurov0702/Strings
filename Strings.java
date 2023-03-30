a2b77319f75e10ba0f56fbab6e2f9f5c4b2b33da
AndreyBurov0702 <129365108+AndreyBurov0702@users.noreply.github.com>
2023-03-30 20:42:20 +0500

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Andrey";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+ " "+"Andrey2");
+        System.out.println("My number is" + x);
+    }
+}
